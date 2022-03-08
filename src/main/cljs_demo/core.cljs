0e285f9d38fdb187b320f1ddbacad8ae306889f1
Sigmund Hansen <sigmund.hansen@uptimeconsulting.no>
2022-03-08 12:38:14 +0100

    Add re-frame with Hello World page and hot-reloading.

--- src/main/cljs_demo/core.cljs
+++ src/main/cljs_demo/core.cljs
@@ -1,4 +1,9 @@
-(ns cljs-demo.core)
+(ns cljs-demo.core
+      (:require [reagent.dom :as reagent-dom]))
 
-(defn init []
-      (println "Hello World!"))
+(defn root []
+  [:h1 "Hello World!"])
+
+(defn ^:dev/after-load init []
+      (reagent-dom/render [root]
+                          (.getElementById js/document "root")))
